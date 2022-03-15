ad712b014d0e5c646e733f7a432f1db72b3eb462
juancamiloed <48024566+juancamiloed@users.noreply.github.com>
2022-03-14 19:58:03 -0500

    Clase JS Operadores

---
+++ Semana 8/Clase 1/src/miCodigo.js
@@ -0,0 +1,28 @@
+console.log('Hola Mundo!!');
+
+let rol;
+
+console.log(rol);
+
+rol = 'estudiante';
+
+const numero = 1
+const string = '1'
+
+console.log(rol);
+
+console.log('Hola soy ' + rol + ' de la UAO'); 
+console.log(`Hola soy ${rol} de la UAO`);
+
+rol === 'estudiante' ? console.log('Eres un estudiante') : console.log('No eres un estudiante');
+
+numero === string ? console.log('Son iguales') : console.log('No son iguales');
+
+let x1 = 35;
+let x2 = 20;
+
+let y1 = x1 / x2
+
+console.log(y1)
+console.log(Math.round(y1))
+console.log(Math.ceil(y1))
